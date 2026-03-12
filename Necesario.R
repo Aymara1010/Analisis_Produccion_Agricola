2687c5307256763ad9c3173cc84b9d70d4aead82
Asly0810 <aslycaputoph@gmail.com>
2026-03-12 11:55:55 +0000

    enlace con r

--- Necesario.R
+++ Necesario.R
@@ -5,8 +5,8 @@ install.packages("gitcreds")
 ## Cargamos libreria y configuramos
 library(usethis)
 use_git_config(
-  user.name = "UsarioGithub", # Cambia esto por tu nombre
-  user.email = "correogithub@gmail.com" # Usa el email asociado a tu cuenta de GitHub
+  user.name = "Asly0810", # Cambia esto por tu nombre
+  user.email = "aslycaputoph@gmail.com" # Usa el email asociado a tu cuenta de GitHub
 )
 
 ## Creamos token para credenciales de github
@@ -15,6 +15,7 @@ create_github_token()
 ## Configuramos token
 library(gitcreds)
 gitcreds_set()
+
 
 
 ## Committ
