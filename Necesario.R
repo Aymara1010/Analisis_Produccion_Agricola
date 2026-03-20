882bf37e17f2080f40306429958a64ba0c10fa2e
Aymara1010 <aymaraandersen2007@gmail.com>
2026-03-20 01:14:56 -0400

    Correcciones + limpieza de datos en R

--- Necesario.R
+++ Necesario.R
@@ -1,12 +1,12 @@
 ## Instalamos librerias necesarias 
 install.packages("usethis")
-install.packages("tidyverse")
+install.packages("gitcreds")
 
 ## Cargamos libreria y configuramos
 library(usethis)
 use_git_config(
-  user.name = "Asly0810", # Cambia esto por tu nombre
-  user.email = "aslycaputoph@gmail.com" # Usa el email asociado a tu cuenta de GitHub
+  user.name = "Aymara1010'", # Cambia esto por tu nombre
+  user.email = "aymaraandersen2007@gmail.com" # Usa el email asociado a tu cuenta de GitHub
 )
 
 ## Creamos token para credenciales de github
